a7e176c25c8395d23770785d3f04334b83e9fa58
Jonathan Gillett <gsoc.student@gmail.com>
2020-05-11 22:27:48 -0400

    Update SDK

--- src/crypto_weather/api/private.clj
+++ src/crypto_weather/api/private.clj
@@ -90,6 +90,28 @@
   ([symbol interval optional-params]
    (:data (v1-private-forecast-symbol-interval-get-with-http-info symbol interval optional-params))))
 
+(defn v1-private-forecast-time-symbol-interval-period-get-with-http-info
+  "Forecast Time
+  "
+  ([symbol interval period ] (v1-private-forecast-time-symbol-interval-period-get-with-http-info symbol interval period nil))
+  ([symbol interval period {:keys [cookie x-csrf ]}]
+   (check-required-params symbol interval period)
+   (call-api "/v1/private/forecast-time/{symbol}/{interval}/{period}" :get
+             {:path-params   {"symbol" symbol "interval" interval "period" period }
+              :header-params {"Cookie" cookie "X-csrf" x-csrf }
+              :query-params  {}
+              :form-params   {}
+              :content-types []
+              :accepts       ["application/json"]
+              :auth-names    ["oauth2"]})))
+
+(defn v1-private-forecast-time-symbol-interval-period-get
+  "Forecast Time
+  "
+  ([symbol interval period ] (v1-private-forecast-time-symbol-interval-period-get symbol interval period nil))
+  ([symbol interval period optional-params]
+   (:data (v1-private-forecast-time-symbol-interval-period-get-with-http-info symbol interval period optional-params))))
+
 (defn v1-private-trend-symbol-get-with-http-info
   "Trend
   The trend response contains a collection of forecasts for different intervals with the following attributes.
